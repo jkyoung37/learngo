523c1af2b23cd02ec45e68e7e0063f71ada4e6cd
DESKTOP-5QB9KIT\jks55 <jkyoung37@gmail.com>
2021-04-27 18:59:41 +0900

    #1.3 Functions part One (08:40)

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
+func repeatME(words ...string) {
+
+	fmt.Println(words)
+}
 
 func main() {
+	repeatME("2", "3", "4", "5")
 
-	var name string = "nico"
-	name = "lynn"
-	fmt.Println(name)
 }
