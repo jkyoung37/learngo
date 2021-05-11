9022daa35cb663f1a2b23c2641727701306413c3
DESKTOP-5QB9KIT\jks55 <jkyoung37@gmail.com>
2021-05-11 18:10:59 +0900

    #1.6 If with a Twist (03:59)

--- main.go
+++ main.go
@@ -2,17 +2,13 @@ package main
 
 import "fmt"
 
-func superApp(numbers ...int) int {
-
-	total := 0
-	for _, number := range numbers {
-		total = total + number
+func canIDrink(age int) bool {
+	if koreanAge := age + 2; koreanAge < 18 {
+		return false
 	}
-
-	return total
+	return true
 }
 
 func main() {
-	result := superApp(1, 2, 3, 4, 5, 6)
-	fmt.Println(result)
+	fmt.Println(canIDrink(16))
 }
