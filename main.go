d491cc05defc709507158f15d649bf11f84bea16
DESKTOP-5QB9KIT\jks55 <jkyoung37@gmail.com>
2021-05-11 18:26:44 +0900

    #1.8 Pointers! (07:24)

--- main.go
+++ main.go
@@ -2,13 +2,11 @@ package main
 
 import "fmt"
 
-func canIDrink(age int) bool {
-	if koreanAge := age + 2; koreanAge < 18 {
-		return false
-	}
-	return true
-}
-
 func main() {
-	fmt.Println(canIDrink(16))
+
+	a := 2
+	b := &a
+
+	fmt.Println(&a, *b)
+
 }
