aa10ce5dc39cde3db8b5d15b3fb769017a1df919
DESKTOP-5QB9KIT\jks55 <jkyoung37@gmail.com>
2021-05-16 15:38:07 +0900

    #1.11 Structs (06:38)

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import "fmt"
 
+type person struct {
+	name    string
+	age     int
+	favFood []string
+}
+
 func main() {
+	favFood := []string{"kimchi", "ramen"}
+	nico := person{name: "nico", age: 18, favFood: favFood}
+	fmt.Println(nico)
 
-	names := []string{"ni", "ci", "ki"}
-	names = append(names, "ka")
-
-	fmt.Println(names)
 }
