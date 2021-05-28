0bbcada3b562126bfcc204a93686c1117291abcc
DESKTOP-5QB9KIT\jks55 <jkyoung37@gmail.com>
2021-05-28 19:47:44 +0900

    #2.6 Update Delete (08:00)

--- main.go
+++ main.go
@@ -8,17 +8,14 @@ import (
 
 func main() {
 	dictionary := mydict.Dictionary{}
-	word := "hello"
-	definition := "Greeting"
-	err := dictionary.Add(word, definition)
-	if err != nil {
-		fmt.Println(err)
-	}
+	baseWord := "hello"
 
-	hello, _ := dictionary.Search(word)
-	fmt.Println(hello)
-	err2 := dictionary.Add(word, definition)
-	if err2 != nil {
-		fmt.Println(err2)
+	dictionary.Add(baseWord, "First")
+	dictionary.Search(baseWord)
+	dictionary.Delete(baseWord)
+	word, err := dictionary.Search(baseWord)
+	if err != nil {
+		fmt.Print(err)
 	}
+	fmt.Println(word)
 }
